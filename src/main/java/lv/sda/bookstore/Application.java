2f92ffffdf89d88c83a3189e1295b2d154added9
Diaxa <pandiaxa@gmail.com>
2021-11-06 13:44:07 +0200

    ar number pārbaudem bez isbn

--- src/main/java/lv/sda/bookstore/Application.java
+++ src/main/java/lv/sda/bookstore/Application.java
@@ -1,4 +1,5 @@
 package lv.sda.bookstore;
+
 import java.time.LocalDate;
 import java.util.Scanner;
 
@@ -9,56 +10,56 @@ public class Application {
 
         Bookstore bookstore = new Bookstore();
 
-        bookstore.addBook(new Book("Klara and the Sun", "Kazuo Ishiguro", LocalDate.of(2021, 1,1), 320, "New York times bestseller", "Ballantine Books", "9780593318171"));
-        bookstore.addBook(new Book("Effortless", "Greg McKeown", LocalDate.of(2021, 1,1), 320, "New York times bestseller", "Ballantine Books", "9780593135648"));
-        bookstore.addBook(new Book("Gold Diggers", "Sanjena Sathian", LocalDate.of(2021, 1,1), 320, "New York times bestseller", "Ballantine Books", "9781984882035"));
-        bookstore.addBook(new Book("Luck of the Titanic", "Stacey Lee", LocalDate.of(2021, 1,1), 320, "New York times bestseller", "Ballantine Books", "9781524740986 "));
-        bookstore.addBook(new Book("Before She Disappeared", "Lisa Gardner", LocalDate.of(2021, 1,1), 320, "New York times bestseller", "Ballantine Books", "9781524745073"));
-        bookstore.addBook(new Book("Four Hundred Souls", "Ibram X. Kendi and Keisha N. Blain", LocalDate.of(2021, 1,1), 320, "New York times bestseller", "Ballantine Books", "9780593134047"));
-        bookstore.addBook(new Book("People We Meet on Vacation", "Emily Henry", LocalDate.of(2021, 1,1), 320, "New York times bestseller", "Ballantine Books", "9781984806758"));
-        bookstore.addBook(new Book("Crying in H Mart", "Michelle Zauner", LocalDate.of(2021, 1,1), 320, "New York times bestseller", "Ballantine Books", "9780525657743"));
-        bookstore.addBook(new Book("Malibu Rising", "Taylor Jenkins Reid", LocalDate.of(2021, 1,1), 320, "New York times bestseller", "Ballantine Books", "9781524798659"));
+        bookstore.addBook(new Book("Klara and the Sun", "Kazuo Ishiguro", LocalDate.of(2021, 1, 1), 320, "New York times bestseller", "Ballantine Books", "9780593318171"));
+        bookstore.addBook(new Book("Effortless", "Greg McKeown", LocalDate.of(2021, 1, 1), 320, "New York times bestseller", "Ballantine Books", "9780593135648"));
+        bookstore.addBook(new Book("Gold Diggers", "Sanjena Sathian", LocalDate.of(2021, 1, 1), 320, "New York times bestseller", "Ballantine Books", "9781984882035"));
+        bookstore.addBook(new Book("Luck of the Titanic", "Stacey Lee", LocalDate.of(2021, 1, 1), 320, "New York times bestseller", "Ballantine Books", "9781524740986 "));
+        bookstore.addBook(new Book("Before She Disappeared", "Lisa Gardner", LocalDate.of(2021, 1, 1), 320, "New York times bestseller", "Ballantine Books", "9781524745073"));
+        bookstore.addBook(new Book("Four Hundred Souls", "Ibram X. Kendi and Keisha N. Blain", LocalDate.of(2021, 1, 1), 320, "New York times bestseller", "Ballantine Books", "9780593134047"));
+        bookstore.addBook(new Book("People We Meet on Vacation", "Emily Henry", LocalDate.of(2021, 1, 1), 320, "New York times bestseller", "Ballantine Books", "9781984806758"));
+        bookstore.addBook(new Book("Crying in H Mart", "Michelle Zauner", LocalDate.of(2021, 1, 1), 320, "New York times bestseller", "Ballantine Books", "9780525657743"));
+        bookstore.addBook(new Book("Malibu Rising", "Taylor Jenkins Reid", LocalDate.of(2021, 1, 1), 320, "New York times bestseller", "Ballantine Books", "9781524798659"));
 
-       while (true){
-           System.out.println("Bookstore menu");
-           System.out.println("1. Search a book");
-           System.out.println("2. Add a book");
-           System.out.println("3. Remove a book");
-           System.out.println("4. Book list");
-           System.out.println("To quit press q");
-           String input = scanner.next();
-           if(input.equals("q")) {
-               System.out.println("Quitting application");
-               break;
-           }
+        while (true) {
+            System.out.println("Bookstore menu");
+            System.out.println("1. Search a book");
+            System.out.println("2. Add a book");
+            System.out.println("3. Remove a book");
+            System.out.println("4. Book list");
+            System.out.println("To quit press q");
+            String input = scanner.next();
+            if (input.equals("q")) {
+                System.out.println("Quitting application");
+                break;
+            }
 
 
-           switch (input){
-               case "1":
-                   System.out.println("Searching for a book by title");
-                   bookstore.searchByTitle();
-                   break;
-               case "2":
-                   System.out.println("Add a book");
-                   Book book = addBook();
-                   bookstore.addBook(book);
-                   break;
-               case "3":
-                   System.out.println("Remove a book");
-                   bookstore.removeBookByISBN();
+            switch (input) {
+                case "1":
+                    System.out.println("Searching for a book by title");
+                    bookstore.searchByTitle();
+                    break;
+                case "2":
+                    System.out.println("Add a book");
+                    Book book = addBook();
+                    bookstore.addBook(book);
+                    break;
+                case "3":
+                    System.out.println("Remove a book");
+                    bookstore.removeBookByISBN();
 
-                   break;
-               case "4":
-                   System.out.println("List of books:");
-                   bookstore.getBookshelf().forEach(System.out::println);
-                   break;
-               default:
-                   System.out.println("Wrong input, please try again!");
-           }
-       }
+                    break;
+                case "4":
+                    System.out.println("List of books:");
+                    bookstore.getBookshelf().forEach(System.out::println);
+                    break;
+                default:
+                    System.out.println("Wrong input, please try again!");
+            }
+        }
     }
 
-    public static Book addBook(){
+    public static Book addBook() {
         Book book = new Book();
         scanner.nextLine();
         System.out.println("Enter title");
@@ -66,12 +67,10 @@ public class Application {
         System.out.println("Enter author");
         book.setAuthor(scanner.nextLine());
         System.out.println("Enter publishing year");
-        int year = Integer.parseInt(scanner.nextLine());
-        LocalDate publishingYear = LocalDate.of(year,1,1);
+        LocalDate publishingYear = inputYear();
         book.setPublishingYear(publishingYear);
         System.out.println("Enter number of pages");
-        book.setPages(scanner.nextInt());
-        scanner.nextLine();
+        book.setPages(inputPages());
         System.out.println("Enter the publisher");
         book.setPublisher(scanner.nextLine());
         System.out.println("Enter description of the book");
@@ -81,5 +80,38 @@ public class Application {
         return book;
     }
 
+    public static LocalDate inputYear() {
+        while (true) {
+            int year = inputNumber();
+            LocalDate publishingYear = LocalDate.of(year, 1, 1);
 
+            if (publishingYear.isBefore(LocalDate.now()) && publishingYear.isAfter(LocalDate.of(1, 1, 1))) {
+                return LocalDate.of(year, 1, 1);
+            } else {
+                System.out.println("Invalid year, try again.");
+            }
+        }
+    }
+
+    public static Integer inputNumber() {
+        while (true) {
+            try {
+                return Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Not a number, try again.");
+            }
+        }
+    }
+
+    public static Integer inputPages() {
+        int pages = inputNumber();
+
+        while (pages <= 0) {
+            System.out.println("Please enter positive number.");
+            pages = inputNumber();
+
+        }
+
+        return pages;
+    }
 }
--- src/main/java/lv/sda/bookstore/Bookstore.java
+++ src/main/java/lv/sda/bookstore/Bookstore.java
@@ -1,5 +1,6 @@
 package lv.sda.bookstore;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -7,49 +8,71 @@ import java.util.Scanner;
 public class Bookstore {
     private List<Book> bookshelf = new ArrayList<>();
 
-    public void addBook(Book book){
-        bookshelf.add(book);
+    public void addBook(Book newBook) {
+        List<Book> foundBooks = new ArrayList<>();
+
+        for (Book book : bookshelf) {
+            if (book.getIsbn().equals(newBook.getIsbn())) {
+                System.out.println("Book already exists in the database.");
+                return;
+            }
+        }
+        bookshelf.add(newBook);
     }
+
 
     public boolean removeBook(String isbn) {
         return bookshelf.removeIf(book -> book.getIsbn().equals(isbn));
-        
+
     }
-    public void listBooks(){
+
+    public void listBooks() {
 
         bookshelf.forEach(System.out::println);
     }
-    public List<Book> getBookshelf(){
+
+    public List<Book> getBookshelf() {
 
         return bookshelf;
     }
-    public List<Book> searchBookByTitle(String query){
+
+    public List<Book> searchBookByTitle(String query) {
 
         List<Book> foundBooks = new ArrayList<>();
 
-        for (Book book : bookshelf){
-            if(book.getTitle().contains(query)){
+        for (Book book : bookshelf) {
+            if (book.getTitle().toLowerCase().contains(query)) {
                 System.out.println(book.getTitle());
                 foundBooks.add(book);
             }
         }
         return foundBooks;
     }
+
     public void searchByTitle() {
         Scanner scan = new Scanner(System.in);
         System.out.println("Please enter book`s title");
         String query = scan.nextLine();
-        searchBookByTitle(query);
+        List<Book> books = searchBookByTitle(query.toLowerCase());
+        if (books.isEmpty()) {
+            System.out.println("No books found.");
+
+        }
     }
+
+
+
+
+
 
     public void removeBookByISBN() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter ISBN of a book you want to remove:");
         String query = scanner.nextLine();
         boolean removed = removeBook(query); // query (lietotāja ievade)-> removeBook (metodei) -> removed (true/false atkarībā vai grāmata tika izdzēsta)
-        if(removed) {
+        if (removed) {
             System.out.println("Book has been removed!");
-        }else{
+        } else {
             System.out.println("Book has not been removed!");
         }
 
